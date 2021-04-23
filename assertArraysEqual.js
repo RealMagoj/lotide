af9288eab1380bc0a0bcd6221f09db7d51725a16
Dom Golac <domgolac@gmail.com>
2021-04-23 04:47:25 +0000

    create assertArraysEqual.js function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected)=> {
-  if (actual === expected) {
+const assertArraysEqual = (actual, expected)=> {
+  if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
   } else {
     console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
@@ -20,7 +20,7 @@ const eqArrays = (array1, array2)=> {
   }
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
