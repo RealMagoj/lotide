bcade0ecf4307b8e32dd3954703695c2e511b2ce
Dom Golac <domgolac@gmail.com>
2021-04-24 05:41:21 +0000

    create assertObjectsEqual.js function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,48 @@
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: "${inspect(actual)}" === "${inspect(expected)}"`);
+  } else {
+    console.log(`🛑 Assertion Failed: "${inspect(actual)}" !== "${inspect(expected)}"`);
+  }
+};
+
+const eqArrays = (array1, array2)=> {
+  if (array1.length === array2.length) {
+    for (let item in array1) {
+      if (array1[item] !== array2[item]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
